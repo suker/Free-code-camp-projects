e71c17a3bff8392571815962cc82af0551e64ac3
Juan <juan@Juans-MacBook-Pro.local>
2020-02-16 10:26:37 +0000

    changing some stuffs

---
+++ FrontEndLibraries/randomQuoteMachine/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import QuoteMachine from './QuoteMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
